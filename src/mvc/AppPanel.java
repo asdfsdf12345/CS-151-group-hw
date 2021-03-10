9cdf49a6a2f82ad48fff3e041e15930945ee13c2
asdfsdf12345 <72778737+asdfsdf12345@users.noreply.github.com>
2021-03-10 15:33:27 -0800

    fixed naming error

---
+++ src/mvc/AppPanel.java
@@ -0,0 +1,13 @@
+package mvc;
+
+import javax.swing.text.View;
+import java.util.*;
+
+
+public class AppPanel {
+    protected Model model;
+    protected View view;
+    protected Jpanel controlPanel;
+    protected AppFactory factory;
+}
+
