3f64434b6f61a4b815778d51ef3a2984240127ee
asdfsdf12345 <72778737+asdfsdf12345@users.noreply.github.com>
2021-03-22 16:24:54 -0700

    most of mvc and some parts of minefield

--- src/mvc/Command.java
+++ src/mvc/Command.java
@@ -1,4 +1,15 @@
 package mvc;
 
 public class Command {
+
+    protected Model model=new Model();
+
+    public Command(Model model){
+        this.model=model;
+    }
+
+    public void execute(){
+
+    }
+
 }
