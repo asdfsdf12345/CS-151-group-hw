638f2de6db504138c5ffaf082854b22adcb87265
asdfsdf12345 <72778737+asdfsdf12345@users.noreply.github.com>
2021-03-22 14:13:10 -0700

    corrections

--- src/mineField/MineFieldFactory.java
+++ src/mineField/MineFieldFactory.java
@@ -3,13 +3,13 @@ package mineField;
 import mvc.*;
 
 public class MineFieldFactory implements AppFactory{
-    
+
     public Model makeModel(){
         return new MineField();
     };
 
     public View makeView(Model m){
-        
+
         return new MineFieldView((MineField)m);
     };
 
@@ -22,11 +22,11 @@ public class MineFieldFactory implements AppFactory{
     };
 
     public String getTitle(){
-        
+        return ("Mine Field");
     };
 
     public String[] getHelp(){
-
+        return new String[] {"Click buttons to move. Do not hit mines. Each tile will show the amount of mines adjacent to it."};
     };
 
     public String about(){
--- src/mvc/View.java
+++ src/mvc/View.java
@@ -1,5 +1,7 @@
 package mvc;
 
-public class View {
-    
+import javax.swing.*;
+
+public class View extends JPanel {
+
 }
