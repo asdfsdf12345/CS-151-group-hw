784538251e7a92d968f0e4efc20ee0eed21c1c86
Kincade <unbeatableness@gmail.com>
2021-03-22 17:47:48 -0700

    Update AppPanel.java

--- src/mvc/AppPanel.java
+++ src/mvc/AppPanel.java
@@ -3,14 +3,9 @@ package mvc;
 import tools.Utilities;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.awt.event.*;
+import java.beans.*;
+import java.io.*;
 import javax.swing.*;
 //import javax.swing.text.Utilities;
 
@@ -26,9 +21,10 @@ public class AppPanel extends JPanel implements ActionListener, PropertyChangeLi
     public static int FRAME_HEIGHT = 300;
 
     public AppPanel(AppFactory factory) {
+        super();
         this.factory = factory;
         model = factory.makeModel();
-        JPanel controlPanel = new JPanel();
+        controlPanel = new JPanel();
         view = factory.makeView(model);
 
         setLayout((new GridLayout(1, 2)));
@@ -38,6 +34,10 @@ public class AppPanel extends JPanel implements ActionListener, PropertyChangeLi
         controlPanel.setBackground(Color.PINK);
         view.setBackground(Color.GRAY);
 
+        if(model != null) {
+            model.addPropertyChangeListener(this);
+            model.addPropertyChangeListener(view);
+        }
 
         frame = new JFrame();
         Container cp = frame.getContentPane();
@@ -46,17 +46,19 @@ public class AppPanel extends JPanel implements ActionListener, PropertyChangeLi
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle(factory.getTitle());
         frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+
+
     }
 
     protected JMenuBar createMenuBar() {
         JMenuBar result = new JMenuBar();
         // add file, edit, and help menus
         JMenu fileMenu =
-                Utilities.makeMenu("File", new String[] {"New",  "Save", "SaveAs", "Open", "Quit"} , this);
+                Utilities.makeMenu("File", new String[] {"New",  "Save", "SaveAs", "Open", "Quit"}, this);
         result.add(fileMenu);
 
         JMenu editMenu =
-                Utilities.makeMenu("Edit", factory.getEditCommands(), this);
+                Utilities.makeMenu("Edit", new String[] {"Change"}, this);
         result.add(editMenu);
 
         JMenu helpMenu =
@@ -72,35 +74,20 @@ public class AppPanel extends JPanel implements ActionListener, PropertyChangeLi
         try {
             String cmmd = ae.getActionCommand();
             if (cmmd == "Save") {
-                try {
-                    //String fName = Utilities.ask("File Name?");
-                    String fName = Utilities.getFileName(null, false);
-                    ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(fName));
-                    os.writeObject(model);
-                    os.close();
-                } catch (Exception err) {
-                    Utilities.error(err);
-                }
+                Utilities.save(model, false);
             } else if (cmmd == "SaveAs") {
                 Utilities.save(model, true);
             } else if (cmmd == "Open") {
-                try {
-                    String fName = Utilities.getFileName(null, true);
-                    ObjectInputStream is = new ObjectInputStream(new FileInputStream(fName));
-                    //model.removePropertyChangeListener(this);
-                    model = (Model) is.readObject();
-                    //this.model.initSupport();
-                    //model.addPropertyChangeListener(this);
-                    view.setModel(model);
-                    is.close();
-                } catch (Exception err) {
-                    Utilities.error(err.getMessage());
-                }
+                Model openModel = Utilities.open(model);
+                setModel(openModel);
             } else if (cmmd == "New") {
-                model = factory.makeModel();
-                view.setModel(model);
+                Utilities.saveChanges(model);
+                setModel(factory.makeModel());
+                //model = factory.makeModel();
+                //view.setModel(model);
+                model.setUnsavedChanges(false);
             } else if (cmmd == "Quit") {
-                //Utilities.saveChanges(model);
+                Utilities.saveChanges(model);
                 System.exit(1);
             } else if (cmmd == "About") {
                 Utilities.inform(factory.about());
@@ -123,5 +110,18 @@ public class AppPanel extends JPanel implements ActionListener, PropertyChangeLi
 
     public void propertyChange(PropertyChangeEvent evt) {
 
+    }
+    
+    public Model getModel() {
+        return model;
+    }
+
+    public void setModel(Model m) {
+        this.model.removePropertyChangeListener(this);
+        this.model = m;
+        this.model.initSupport();
+        this.model.addPropertyChangeListener(this);
+        view.setModel(this.model);
+        model.changed();
     }
 }
