1c3e593e79e62df589a213641d52b7c3c2d90e83
kat-rose6 <80294939+kat-rose6@users.noreply.github.com>
2021-03-22 20:37:53 -0700

    Update MineFieldView.java

--- src/mineField/MineFieldView.java
+++ src/mineField/MineFieldView.java
@@ -2,9 +2,33 @@ package mineField;
 
 import mvc.*;
 
-public class MineFieldView extends View {
+import javax.swing.*;
+import java.awt.*;
+import java.beans.PropertyChangeEvent;
 
-    public MineFieldView(Model m) {
-        
+public class MineFieldView extends View {
+    protected MineField mineField;
+    protected JLabel[][] grid;
+    private int N = 20;
+
+    public MineFieldView (MineField m) {
+        super(m);
+        mineField = m;
+        grid = new JLabel[N][N];
+        for (int i = 0; i <= N; i++) {
+            for (int j = 0; j <= N; j++) {
+                grid[i][j] = new JLabel("?", SwingConstants.LEFT);
+                grid[i][j].setBackground(Color.GRAY);
+                grid[i][j].setBorder(BorderFactory.createLineBorder(Color.BLACK));
+            }
+        }
+    }
+
+
+    @Override
+    public void propertyChange(PropertyChangeEvent arg0) {
+        super.propertyChange(arg0);
+        grid(mineField.pos).setBorder(BorderFactory.createLineBorder(Color.WHITE));
     }
 }
+
